3ef1eeefff13caafb7ed03042e0d637350b88dc8
ahenrici <ahenrici@email.arizona.edu>
2018-04-16 18:47:45 -0700

    added RotateFrame.py

---
+++ Project/RotateFrame.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+'''
+File name:  CenterOfMass.py
+Author: Andrew Henrici
+Date Creaated:  3/23/2018
+Date last modified: 3/23/2018
+Description:  function to rotate the position and
+velocity vectors of a galaxy to line up the galaxy's plane
+with the world's coordinates.
+'''
+
+# import modules
+import numpy as np
+import astropy.units as u
+from astropy.constants import G
+
+# import plotting modules
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+# my modules
+from ReadFile import Read
+from CenterOfMass import CenterOfMass
+
+def RotateFrame(posI,velI):
+    # input:  3D array of positions and velocities
+    # returns: 3D array of rotated positions and velocities such that j is in z direction
+
+    # compute the angular momentum
+    L = np.sum(np.cross(posI,velI), axis=0)
+    # normalize the vector
+    L_norm = L/np.sqrt(np.sum(L**2))
+
+
+    # Set up rotation matrix to map L_norm to z unit vector (disk in xy-plane)
+    
+    # z unit vector
+    z_norm = np.array([0, 0, 1])
+    
+    # cross product between L and z
+    vv = np.cross(L_norm, z_norm)
+    s = np.sqrt(np.sum(vv**2))
+    
+    # dot product between L and z 
+    c = np.dot(L_norm, z_norm)
+    
+    # rotation matrix
+    I = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    v_x = np.array([[0, -vv[2], vv[1]], [vv[2], 0, -vv[0]], [-vv[1], vv[0], 0]])
+    R = I + v_x + np.dot(v_x, v_x)*(1 - c)/s**2
+
+    # Rotate coordinate system
+    pos = np.dot(R, posI.T).T
+    vel = np.dot(R, velI.T).T
+    
+    return pos, vel
+
